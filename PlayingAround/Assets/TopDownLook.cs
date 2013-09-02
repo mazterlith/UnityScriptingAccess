55bc971180bd9bec1719bd0d5d70c6f7b317fdde
mazterlith <benjamin.mcdonald@outlook.com>
2013-09-02 12:32:02 -0700

    Added new scene

---
+++ PlayingAround/Assets/TopDownLook.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TopDownLook : MonoBehaviour
+{
+	public Transform target;
+	
+	void Update ()
+	{
+		//On update, change the x and z coordinates to whatever the target's is, but keep the y coordinate.
+		this.transform.position = new Vector3(target.position.x, this.transform.position.y, target.position.z);
+	}
+}
---
+++ PlayingAround/Assets/TwoDimMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TwoDimMovement : MonoBehaviour
+{
+	public float speed;
+	
+	void FixedUpdate ()
+	{
+		if(Input.GetKey(KeyCode.UpArrow))
+			this.rigidbody.AddForce(this.transform.forward * speed * Time.fixedDeltaTime);
+			//this.transform.position += this.transform.forward * speed * Time.fixedDeltaTime;
+		
+		if(Input.GetKey(KeyCode.DownArrow))
+			this.rigidbody.AddForce(-this.transform.forward * speed * Time.fixedDeltaTime);
+			//this.transform.position -= this.transform.forward * speed * Time.fixedDeltaTime;
+		
+		if(Input.GetKey(KeyCode.RightArrow))
+			this.rigidbody.AddForce(this.transform.right * speed * Time.fixedDeltaTime);
+			//this.transform.position += this.transform.right * speed * Time.fixedDeltaTime;
+		
+		if(Input.GetKey(KeyCode.LeftArrow))
+			this.rigidbody.AddForce(-this.transform.right * speed * Time.fixedDeltaTime);
+			//this.transform.position -= this.transform.right * speed * Time.fixedDeltaTime;
+	}
+}
